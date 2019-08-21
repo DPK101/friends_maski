7eccd1889be70b11df2376bf186566fc3e368d84
Deepak <DPK101@users.noreply.github.com>
2019-08-21 09:16:19 +0530

    Updates

---
+++ precache-manifest.c85a44ae346aa27f1bb770a446a156c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0235d97fe4a92a7a8a123a985f9520f6",
+    "url": "/friends_maski/index.html"
+  },
+  {
+    "revision": "2adb24cd2d30dd6779af",
+    "url": "/friends_maski/static/css/main.47d712dc.chunk.css"
+  },
+  {
+    "revision": "0d8cf3cc5fe58a6634fa",
+    "url": "/friends_maski/static/js/2.fc0bb13f.chunk.js"
+  },
+  {
+    "revision": "2adb24cd2d30dd6779af",
+    "url": "/friends_maski/static/js/main.4a2ece8e.chunk.js"
+  },
+  {
+    "revision": "1e4655dfab91c2d1d91b",
+    "url": "/friends_maski/static/js/runtime~main.18aa0d2f.js"
+  }
+]);
